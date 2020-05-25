4714cef95e921b1d9bab1409e5603e91304a3e12
dalecoin <57284937+dalecoin@users.noreply.github.com>
2020-05-26 00:30:46 +0100

    Update rpcblockchain.cpp

--- src/rpc/rpcblockchain.cpp
+++ src/rpc/rpcblockchain.cpp
@@ -288,7 +288,7 @@ Value getblock(const Array& params, bool fHelp)
     if (params.size() > 1) {
             verbosity = params[1].get_bool() ? 1 : 0;
     }else{
-	    verbosity = 0;	//Output RAW TX if second parameter is not set, useful for ElectrumX
+	    verbosity = 1;	//Output RAW TX if second parameter is not set, useful for ElectrumX
     }
 
     if (mapBlockIndex.count(hash) == 0)
